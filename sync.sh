4cd2fbd92253aea6a22d34844b7fc54714f12afb
Dan Peterson <dpiddy@gmail.com>
2011-08-22 14:47:20 -0300

    Rename to ledger-sync and ledger-tidy.

--- sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-./tidy.sh
-git commit -a -m 'sync'
-git push origin master
--- tidy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-ledger print --sort d > finances.ledger.new && mv finances.ledger.new finances.ledger
